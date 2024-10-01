80a8bd4aa913a4564d00fb17f866b7b8044da53e
“JorgeAlfredoLozano” <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2024-10-01 10:53:03 -0300

    solucion 2

--- client/src/components/Pages/Asistencia/Asistencia.jsx
+++ client/src/components/Pages/Asistencia/Asistencia.jsx
@@ -8,33 +8,29 @@ import CotizarButton from "./Seccion1/CotizarButton";
 import WhatsApp from "./WhatsApp";
 
 const Asistencia = () => {
-  const formRef = useRef(null); // Creamos la referencia al formulario
+  const formRef = useRef(null); // Referencia al formulario
 
   const scrollToForm = () => {
-    formRef.current?.scrollIntoView({ behavior: "smooth" }); // Acción de scroll suave
+    formRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
-    window.scrollTo(0, 0); 
+    window.scrollTo(0, 0);
   }, []);
 
   return (
     <div>
+      {/* Sección de la imagen de fondo */}
       <div
-        className="relative flex flex-col justify-center items-center w-full"
+        className="relative flex flex-col justify-center items-center w-full h-[80vh] bg-cover bg-center"
         style={{
           backgroundImage: `url('https://images.pexels.com/photos/5225325/pexels-photo-5225325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          width: "100%",
-          height: "100vh",
-          maxHeight: "80vh",
         }}
       >
         {/* Overlay para oscurecer la imagen */}
         <div className="absolute inset-0 bg-black opacity-40" />
 
-        {/* Contenedor para el texto y el botón */}
+        {/* Contenido centrado */}
         <div className="absolute top-20 flex flex-col items-center text-center px-4 space-y-4">
           <h1 className="text-white text-4xl md:text-6xl lg:text-8xl font-bold">
             CONTRATÁ AHORA TU ASISTENCIA
@@ -42,7 +38,7 @@ const Asistencia = () => {
           <h2 className="text-[#0aeb08] text-3xl md:text-5xl lg:text-6xl font-bold">
             ¡APROVECHA LOS DESCUENTOS Y FACILIDADES DE PAGO!
           </h2>
-          <h3 className="text-gray-300 text-3xl md:text-3xl lg:text-3xl font-bold">
+          <h3 className="text-gray-300 text-3xl font-bold">
             Aboná en PESOS con todos los medios de pago
           </h3>
           <p className="text-white text-lg md:text-xl lg:text-3xl mx-auto max-w-5xl">
@@ -50,7 +46,7 @@ const Asistencia = () => {
             respaldo de un seguro de viaje y una asistencia al viajero!
           </p>
 
-          {/* Botón modificado con acción de scroll */}
+          {/* Botón que hace scroll al formulario */}
           <button
             className="bg-[#0aeb08] text-white text-lg md:text-xl lg:text-2xl px-4 md:px-6 py-2 rounded-lg"
             onClick={scrollToForm}
@@ -60,20 +56,16 @@ const Asistencia = () => {
         </div>
       </div>
 
+      {/* Sección de información y tarjetas */}
       <div className="flex flex-col items-center py-10 bg-white shadow-md rounded-lg mt-4 mx-4 md:mx-8 lg:mx-16">
         <h2 className="text-4xl font-bold mb-8">
           ¿Cuánto cuesta la asistencia médica en el extranjero?
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full px-4">
-          {/* Tarjetas aquí */}
-          {/* Tarjeta 1 */}
+          {/* Tarjetas */}
           <div className="bg-white shadow-lg rounded-lg overflow-hidden border border-gradient-to-r from-green-400 to-blue-500 hover:shadow-2xl transition-shadow duration-300">
             <div className="flex justify-center items-center h-32 bg-gray-200">
-              <Flag
-                country="BR"
-                size={128}
-                className="text-5xl text-green-500"
-              />
+              <Flag country="BR" size={128} className="text-5xl text-green-500" />
             </div>
             <div className="p-4 text-center">
               <h3 className="text-xl font-bold underline">
@@ -86,14 +78,9 @@ const Asistencia = () => {
             </div>
           </div>
 
-          {/* Tarjeta 2 */}
           <div className="bg-white shadow-lg rounded-lg overflow-hidden border border-gradient-to-r from-green-400 to-blue-500 hover:shadow-2xl transition-shadow duration-300">
             <div className="flex justify-center items-center h-32 bg-gray-200">
-              <Flag
-                country="US"
-                size={128}
-                className="text-5xl text-green-500"
-              />
+              <Flag country="US" size={128} className="text-5xl text-green-500" />
             </div>
             <div className="p-4 text-center">
               <h3 className="text-xl font-bold underline">
@@ -106,14 +93,9 @@ const Asistencia = () => {
             </div>
           </div>
 
-          {/* Tarjeta 3 */}
           <div className="bg-white shadow-lg rounded-lg overflow-hidden border border-gradient-to-r from-green-400 to-blue-500 hover:shadow-2xl transition-shadow duration-300">
             <div className="flex justify-center items-center h-32 bg-gray-200">
-              <Flag
-                country="EU"
-                size={128}
-                className="text-5xl text-green-500"
-              />
+              <Flag country="EU" size={128} className="text-5xl text-green-500" />
             </div>
             <div className="p-4 text-center">
               <h3 className="text-xl font-bold underline">
@@ -126,13 +108,14 @@ const Asistencia = () => {
             </div>
           </div>
         </div>
+
         <p className="text-gray text-lg md:text-xl lg:text-3xl mx-6 mt-2">
           La contratación de un seguro de viaje no evitará que un accidente,
           robo o enfermedad ocurran, pero si alguna de estas situaciones se
           presenta, contarás con un respaldo inmediato para afrontar el
           inconveniente y continuar con tu itinerario.
         </p>
-        {/* Botón con scroll */}
+
         <button
           className="bg-[#0aeb08] text-white text-lg md:text-xl lg:text-2xl px-4 mt-4 md:px-6 py-2 rounded-lg"
           onClick={scrollToForm}
@@ -144,16 +127,16 @@ const Asistencia = () => {
       {/* Sección del formulario */}
       <div
         className="bg-gray-300 shadow-md rounded-lg mt-4 mx-4 md:mx-8 lg:mx-16"
-        ref={formRef} // Referenciamos el formulario
+        ref={formRef}
       >
-        <h2 className="text-4xl font-bold mb-2 text-center leading-normal">
+        <h2 className="text-4xl font-bold mb-2 text-center">
           Completa el formulario y te contactaré con tu cotización
         </h2>
-        <h3 className="text-red-500 text-2xl font-bold mb-2 text-center leading-normal  md:text-xl lg:text-1xl">
+        <h3 className="text-red-500 text-2xl font-bold mb-2 text-center">
           IMPORTANTE: Si buscas un seguro de bajo costo y con prestaciones
           limitadas, esta cotización no será adecuada para ti.
         </h3>
-        <h3 className="text-gray-500 text-2xl font-bold mb-2 text-center leading-normal  md:text-lg lg:text-1xl">
+        <h3 className="text-gray-500 text-2xl font-bold mb-2 text-center">
           Trabajo con compañías líderes en asistencia de viaje, como Coris, Pax,
           Assist Card, Europ Assistance, Universal Assistance, y Terrawind
           Global Protection, que ofrecen cobertura integral y de alta calidad.
@@ -167,28 +150,26 @@ const Asistencia = () => {
           </div>
         </div>
       </div>
+
+      {/* Información de contacto */}
       <div className="flex flex-col md:flex-row items-center p-4 bg-white shadow-md rounded-lg mt-4 mx-4 md:mx-8 lg:mx-16">
         <p className="text-black text-base md:text-lg lg:text-2xl mt-2">
           Si tienes problemas con el formulario, te dejo mi contacto directo con
           WhatsApp
         </p>
-        <WhatsApp
-          phone={2281531966}
-          msg={"Este mensaje llega por publicidad!"}
-        />
+        <WhatsApp phone={2281531966} msg={"Este mensaje llega por publicidad!"} />
       </div>
 
+      {/* Sección de información personal */}
       <div className="flex flex-col md:flex-row items-center p-4 bg-white shadow-md rounded-lg mt-4 mx-4 md:mx-8 lg:mx-16">
-        {/* Contenedor de la imagen */}
         <div className="flex-shrink-0 mb-4 md:mb-0">
           <img
             src={Laura}
-            alt="Descripción de la imagen"
+            alt="María Laura Lobrundo"
             className="w-34 h-34 md:w-42 md:h-42 lg:w-50 lg:h-50 rounded-full object-cover border-4 border-[#0aeb08]"
           />
         </div>
 
-        {/* Contenedor de texto */}
         <div className="md:ml-4">
           <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-gray-800">
             María Laura Lobrundo
@@ -196,13 +177,10 @@ const Asistencia = () => {
           <p className="text-gray text-base md:text-lg lg:text-2xl mt-2">
             Hola, soy María Laura Lobrundo, Productora Asesora de Seguros (Mat.
             81581), especializada en seguros de viaje y asistencia al viajero.
-            Desde hace años, he ayudado a muchas personas a disfrutar de sus
-            viajes sin preocupaciones.
           </p>
           <p className="text-gray text-base md:text-lg lg:text-2xl mt-2">
             Aunque en muchos casos los seguros de viaje no son obligatorios para
-            ingresar a determinados países, algo es seguro: son absolutamente
-            necesarios.
+            ingresar a determinados países, son absolutamente necesarios.
           </p>
         </div>
       </div>
